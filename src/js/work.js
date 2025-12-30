3bc9816e9ec43683db6d63b25bbf5ee0d6c19a3f
Daniil Skipa <d71011072@gmail.com>
2025-12-30 03:26:12 +0300

    hw-3 done

---
+++ src/js/work.js
@@ -0,0 +1,23 @@
+// 1 Завдання
+const result = 5 + 5 + "5";
+console.log(result);
+console.log(typeof result);
+
+// 2 Завдання
+const email = "d71011072@gmail.com";
+const emaiIncludes = email.includes("@");
+console.log(emaiIncludes);
+console.log(email.length);
+
+// 3 Завдання
+const firstWord = "My";
+const secondWord = "name";
+const thirdWord = "is";
+let fullName = ` ${firstWord} ${secondWord} ${thirdWord} `;
+fullName += "Victor";
+console.log(fullName);
+
+// 4 Завдання
+const userName = "Олександро";
+const payment = "300";
+alert(`«Дякуємо, ${userName}! До сплати ${payment} гривень»`);
